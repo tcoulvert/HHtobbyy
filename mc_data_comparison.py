6e2b9dbf75ca880f34077dd0a334de0392147c5b
Thomas Sievert <tcsievert@gmail.com>
2024-08-05 18:44:21 -0500

    ratio plot broken

--- mc_data_comparison.py
+++ mc_data_comparison.py
@@ -203,8 +203,6 @@ def generate_hists(MC_pqs: dict, Data_pqs: dict, variable, axis, blind_edges=Non
     # https://indico.cern.ch/event/1433936/ #
     # Generate MC hist stack
     mc_hists = {}
-    mc_ak = ak.zip({variable: FILL_VALUE})
-    mc_weights_ak = ak.zip({variable: FILL_VALUE})
     for dir_name, sample in MC_pqs.items():
         # Blinds a region of the plot if necessary
         if blind_edges is not None:
@@ -215,14 +213,6 @@ def generate_hists(MC_pqs: dict, Data_pqs: dict, variable, axis, blind_edges=Non
             var=ak.where(sample['MC_Data_mask'], sample[variable], FILL_VALUE),
             weight=sample['eventWeight']
         )
-        mc_ak[variable] = ak.concatenate(
-            (mc_ak[variable], ak.where(sample['MC_Data_mask'], sample[variable], FILL_VALUE))
-        )
-        mc_weights_ak[variable] = ak.concatenate(
-            (mc_weights_ak[variable], sample['eventWeight'])
-        )
-    mc_stack = hist.Stack.from_dict(mc_hists)
-    # print(f"MC_stack fields = \n{mc_stack.__dir__}")
 
     # Generate data hist
     data_ak = ak.zip({variable: FILL_VALUE})
@@ -237,22 +227,26 @@ def generate_hists(MC_pqs: dict, Data_pqs: dict, variable, axis, blind_edges=Non
     data_hist = hist.Hist(axis).fill(var=data_ak[variable])
 
     # Generate ratio subplot hist
-    # mc_ak = ak.zip({variable: FILL_VALUE})
-    # mc_weights_ak = ak.zip({variable: FILL_VALUE})
-    # for dir_name, sample in MC_pqs.items():
-    #     if blind_edges is not None:
-    #         sample['MC_Data_mask'] = sample['MC_Data_mask'] & (
-    #             (sample[variable] < blind_edges[0]) | (sample[variable] > blind_edges[1])
-    #         )
-    #     mc_ak[variable] = ak.concatenate(
-    #         (mc_ak[variable], ak.where(sample['MC_Data_mask'], sample[variable], FILL_VALUE))
-    #     )
-    #     mc_weights_ak[variable] = ak.concatenate(
-    #         (mc_weights_ak[variable], sample['eventWeight'])
-    #     )
+    # ratio_hist = hist.Hist(axis).fill(
+    #     mc_hists[MC_NAMES_PRETTY[dir_name]] / data_hist
+    # )
+    mc_ak = ak.zip({variable: FILL_VALUE})
+    mc_weights_ak = ak.zip({variable: FILL_VALUE})
+    for dir_name, sample in MC_pqs.items():
+        if blind_edges is not None:
+            sample['MC_Data_mask'] = sample['MC_Data_mask'] & (
+                (sample[variable] < blind_edges[0]) | (sample[variable] > blind_edges[1])
+            )
+        mc_ak[variable] = ak.concatenate(
+            (mc_ak[variable], ak.where(sample['MC_Data_mask'], sample[variable], FILL_VALUE))
+        )
+        mc_weights_ak[variable] = ak.concatenate(
+            (mc_weights_ak[variable], sample['eventWeight'])
+        )
     ratio_hist = hist.Hist(hist.axis.StrCategory([], growth=True, name="cat"), axis, storage='weight')
     ratio_hist.fill(cat="numer", var=mc_ak[variable], weight=mc_weights_ak[variable])
     ratio_hist.fill(cat="denom", var=data_ak[variable])
+    # ratio_hist = mc_stack[0] / data_hist
 
     # fig,axs = plt.subplots(2, 1, sharex=True, height_ratios=[4,1])
 
@@ -270,9 +264,10 @@ def generate_hists(MC_pqs: dict, Data_pqs: dict, variable, axis, blind_edges=Non
     # plt.tight_layout()
     # fig.subplots_adjust(hspace=0.05)
 
-    return mc_stack, data_hist, ratio_hist
+    return mc_hists, data_hist, ratio_hist
+    # return mc_hists, data_hist, None
 
-def plot(variable, mc_hist, data_hist, ratio_hist):
+def plot(variable: str, mc_hist: dict, data_hist: hist.Hist, ratio_hist):
     """
     Plots and saves out the data-MC comparison histogram
     """
@@ -280,11 +275,14 @@ def plot(variable, mc_hist, data_hist, ratio_hist):
     fig, axs = plt.subplots(
         2, 1, sharex=True, height_ratios=[4,1], figsize=(10, 8)
     )
-    mc_hist.plot(
-        stack=True, ax=axs[0], linewidth=3, histtype="fill", sort='yield_r'
+    hep.histplot(
+        list(mc_hist.values()), label=list(mc_hist.keys()), stack=True, ax=axs[0], linewidth=3, histtype="fill", sort="yield_r"
+    )
+    hep.histplot(
+        data_hist, ax=axs[0], linewidth=3, histtype="errorbar", color="black", label=f"CMS Data"
     )
     # hep.histplot(
-    #     data_hist, ax=axs[0], linewidth=3, histtype="errorbar", color="black", label=f"CMS Data"
+    #     mc_hist[0] / data_hist, ax=axs[0], linewidth=3, histtype="errorbar", color="black", label=f"CMS Data"
     # )
     # Make ratio subplot
     ratio_hist[hist.loc("numer"),:].plot_ratio(
@@ -292,7 +290,16 @@ def plot(variable, mc_hist, data_hist, ratio_hist):
         rp_num_label="MC",
         rp_denom_label="Data",
         ax_dict={"main_ax":axs[0],"ratio_ax":axs[1]},
-        colors=['green', 'black']
+        eb_ecolor="red",
+        eb_mfc="green",
+        eb_mec="black",
+        eb_fmt="o",
+        eb_ms=5,
+        fp_color="lightseagreen",
+        pp_color="darkgreen",
+        pp_alpha=0.4,
+        pp_ec=None,
+        bar_color="darkgreen",
     )
     # ratio_hist[hist.loc("numer"),:].plot_ratio(
     #     ratio_hist[hist.loc("denom"),:],
