55ff36a4fd94075e65087e963dfcd63de78a8078
Thomas Sievert <tcsievert@gmail.com>
2025-10-15 04:49:50 -0500

    figured out potential issue

--- preprocessing/resolved_BDT.py
+++ preprocessing/resolved_BDT.py
@@ -24,91 +24,93 @@ BASIC_VARIABLES = lambda jet_prefix: {
     # MET variables
     'puppiMET_pt',
 
-    # jet vars
-    f'{jet_prefix}_DeltaPhi_j1MET', f'{jet_prefix}_DeltaPhi_j2MET', 
-    'n_jets', f'{jet_prefix}_chi_t0', f'{jet_prefix}_chi_t1',
+    # # jet vars
+    # f'{jet_prefix}_DeltaPhi_j1MET', f'{jet_prefix}_DeltaPhi_j2MET', 
+    # 'n_jets', f'{jet_prefix}_chi_t0', f'{jet_prefix}_chi_t1',
 
-    # lepton vars
-    'lepton1_pt', 'lepton1_pfIsoId', 'lepton1_mvaID',
-    'DeltaR_b1l1',
+    # # lepton vars
+    # 'lepton1_pt', 'lepton1_pfIsoId', 'lepton1_mvaID',
+    # 'DeltaR_b1l1',
 
-    # angular vars
-    f'{jet_prefix}_CosThetaStar_CS', f'{jet_prefix}_CosThetaStar_jj', f'{jet_prefix}_CosThetaStar_gg',
+    # # angular vars
+    # f'{jet_prefix}_CosThetaStar_CS', f'{jet_prefix}_CosThetaStar_jj', f'{jet_prefix}_CosThetaStar_gg',
 
-    # bjet vars
-    f'{jet_prefix}_lead_bjet_eta', # eta
-    f"{jet_prefix}_lead_bjet_btagPNetB",
-    # f"{jet_prefix}lead_bjet_btagUParTAK4B",
-    # f"{jet_prefix}_lead_bjet_bTagWPL", f"{jet_prefix}_lead_bjet_bTagWPM", f"{jet_prefix}_lead_bjet_bTagWPT",
-    # f"{jet_prefix}_lead_bjet_bTagWPXT", f"{jet_prefix}_lead_bjet_bTagWPXXT",
-    # --------
-    f'{jet_prefix}_sublead_bjet_eta', 
-    f"{jet_prefix}_sublead_bjet_btagPNetB",
-    # f"{jet_prefix}sublead_bjet_btagUParTAK4B",
-    # f"{jet_prefix}_sublead_bjet_bTagWPL", f"{jet_prefix}_sublead_bjet_bTagWPM", f"{jet_prefix}_sublead_bjet_bTagWPT",
-    # f"{jet_prefix}_sublead_bjet_bTagWPXT", f"{jet_prefix}_sublead_bjet_bTagWPXXT",
+    # # bjet vars
+    # f'{jet_prefix}_lead_bjet_eta', # eta
+    # f"{jet_prefix}_lead_bjet_btagPNetB",
+    # # f"{jet_prefix}lead_bjet_btagUParTAK4B",
+    # # f"{jet_prefix}_lead_bjet_bTagWPL", f"{jet_prefix}_lead_bjet_bTagWPM", f"{jet_prefix}_lead_bjet_bTagWPT",
+    # # f"{jet_prefix}_lead_bjet_bTagWPXT", f"{jet_prefix}_lead_bjet_bTagWPXXT",
+    # # --------
+    # f'{jet_prefix}_sublead_bjet_eta', 
+    # f"{jet_prefix}_sublead_bjet_btagPNetB",
+    # # f"{jet_prefix}sublead_bjet_btagUParTAK4B",
+    # # f"{jet_prefix}_sublead_bjet_bTagWPL", f"{jet_prefix}_sublead_bjet_bTagWPM", f"{jet_prefix}_sublead_bjet_bTagWPT",
+    # # f"{jet_prefix}_sublead_bjet_bTagWPXT", f"{jet_prefix}_sublead_bjet_bTagWPXXT",
     
-    # diphoton vars
-    'eta',
+    # # diphoton vars
+    # 'eta',
 
-    # Photon vars
-    'lead_mvaID', 'lead_sigmaE_over_E',
-    # --------
-    'sublead_mvaID', 'sublead_sigmaE_over_E',
+    # # Photon vars
+    # 'lead_mvaID', 'lead_sigmaE_over_E',
+    # # --------
+    # 'sublead_mvaID', 'sublead_sigmaE_over_E',
     
-    # HH vars
-    f'{jet_prefix}_HHbbggCandidate_pt', 
-    f'{jet_prefix}_HHbbggCandidate_eta', 
-    f'{jet_prefix}_pt_balance',
+    # # HH vars
+    # f'{jet_prefix}_HHbbggCandidate_pt', 
+    # f'{jet_prefix}_HHbbggCandidate_eta', 
+    # f'{jet_prefix}_pt_balance',
 
-    # ZH vars
-    f'{jet_prefix}_DeltaPhi_jj',
-    f'{jet_prefix}_DeltaPhi_isr_jet_z',
+    # # ZH vars
+    # f'{jet_prefix}_DeltaPhi_jj',
+    # f'{jet_prefix}_DeltaPhi_isr_jet_z',
 }
 MHH_CORRELATED_VARIABLES = lambda jet_prefix: {
-    # MET variables
-    'puppiMET_sumEt',  #eft
+    # # MET variables
+    # 'puppiMET_sumEt',  #eft
 
-    # jet vars
-    f'{jet_prefix}_DeltaR_jg_min',  #eft
+    # # jet vars
+    # f'{jet_prefix}_DeltaR_jg_min',  #eft
 
-    # dijet vars
-    f'{jet_prefix}_dijet_mass' + ('' if jet_prefix == 'nonRes' else '_DNNreg'),  #eft
-    f'{jet_prefix}_dijet_pt',  #eft
+    # # dijet vars
+    # f'{jet_prefix}_dijet_mass' + ('' if jet_prefix == 'nonRes' else '_DNNreg'),  #eft
+    # f'{jet_prefix}_dijet_pt',  #eft
 
-    # bjet vars
-    f'{jet_prefix}_lead_bjet_pt', #eft
-    f'{jet_prefix}_lead_bjet_sigmapT_over_pT', #eft
-    f'{jet_prefix}_lead_bjet_pt_over_Mjj', #eft
-    # --------
-    f'{jet_prefix}_sublead_bjet_pt', #eft
-    f'{jet_prefix}_sublead_bjet_sigmapT_over_pT', #eft
-    f'{jet_prefix}_sublead_bjet_pt_over_Mjj', #eft
+    # # bjet vars
+    # f'{jet_prefix}_lead_bjet_pt', #eft
+    # f'{jet_prefix}_lead_bjet_sigmapT_over_pT', #eft
+    # f'{jet_prefix}_lead_bjet_pt_over_Mjj', #eft
+    # # --------
+    # f'{jet_prefix}_sublead_bjet_pt', #eft
+    # f'{jet_prefix}_sublead_bjet_sigmapT_over_pT', #eft
+    # f'{jet_prefix}_sublead_bjet_pt_over_Mjj', #eft
 
     # diphoton vars
     'pt',  #eft
 
-    # ZH vars
-    f'{jet_prefix}_DeltaEta_jj', #eft
-    f'{jet_prefix}_isr_jet_pt',  #eft
+    # # ZH vars
+    # f'{jet_prefix}_DeltaEta_jj', #eft
+    # f'{jet_prefix}_isr_jet_pt',  #eft
 }
 AUX_VARIABLES = lambda jet_prefix: {
     # identifiable event info
-    'event', 'lumi', 'hash', 'sample_name', 
+    'event', 'lumi', 
+    # 'hash', 'sample_name', 
 
     # MC info
-    'weight', 'eventWeight',
+    'weight', 
+    # 'eventWeight',
 
-    # mass
-    'mass', 
-    f'{jet_prefix}_dijet_mass' + ('' if jet_prefix == 'nonRes' else '_DNNreg'),
-    f'{jet_prefix}_HHbbggCandidate_mass',
+    # # mass
+    # 'mass', 
+    # f'{jet_prefix}_dijet_mass' + ('' if jet_prefix == 'nonRes' else '_DNNreg'),
+    # f'{jet_prefix}_HHbbggCandidate_mass',
 
-    # sculpting study
-    f'{jet_prefix}_max_nonbjet_btag',
+    # # sculpting study
+    # f'{jet_prefix}_max_nonbjet_btag',
 
-    # event masks
-    f'{jet_prefix}_resolved_BDT_mask',
+    # # event masks
+    # f'{jet_prefix}_resolved_BDT_mask',
 
 }
 
@@ -120,7 +122,7 @@ SEED = 21
 BASE_FILEPATH = 'Run3_202'
 CURRENT_TIME = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 CWD = os.getcwd()
-DF_MASK = 'default'
+DF_MASK = 'none'  # 'default'
 
 DEBUG = False
 DRYRUN = False
@@ -226,6 +228,8 @@ def make_output_filepath(filepath, base_output_dirpath, extra_text):
     return os.path.join(output_dirpath, filename)
 
 def get_df_mask(df, aux_df):
+    if DF_MASK == 'none':
+        return (df['pt'] > 0)
     if DF_MASK == 'default':
         return (aux_df[f'{JET_PREFIX}_resolved_BDT_mask'] > 0)
     else:
@@ -343,7 +347,7 @@ def preprocess_resolved_bdt(input_filepaths, output_dirpath):
                 print(f"input = \n{filepath}\n{'-'*60}\noutput = \n{output_filepath}")
                 print(f"num events = {len(df)}")
                 print(f"sum of weights = {train_aux_dfs_fold[filepath].loc[:,'weight'].sum()}")
-                print(f"sum of eventWeights = {train_aux_dfs_fold[filepath].loc[:,'eventWeight'].sum()}")
+                # print(f"sum of eventWeights = {train_aux_dfs_fold[filepath].loc[:,'eventWeight'].sum()}")
 
             cols = list(df.columns)
             df = apply_logs(df)
