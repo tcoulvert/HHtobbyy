5084cbde54e546e6603dabfd2ab30d648e4cd4b0
Thomas Sievert <tcsievert@gmail.com>
2025-11-24 17:00:54 -0600

    Commit before training at 2025-11-24_17-00-54

--- training/condor_training.py
+++ training/condor_training.py
@@ -74,6 +74,8 @@ def submit(
         subprocess.run(['git', 'commit', '-a', '-m', f'Commit before training at {CURRENT_TIME}'], check=True, capture_output=True, text=True)
         subprocess.run(['git', 'push'], check=True)
     except subprocess.CalledProcessError as e:
+        print(e.stderr)
+        print('Your branch is up to date with'.lower() not in e.stderr.lower())
         if 'Your branch is up to date with'.lower() not in e.stderr.lower(): 
             logger.error(f"Committing and pushing to git failed")
             raise e
