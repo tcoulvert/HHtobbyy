c31661bf194a33a88e0d508dee4b9433c65ebcc3
Thomas Sievert <tcsievert@gmail.com>
2026-01-30 09:06:36 -0600

    added hist plot to model for plotting final fit

--- categorization/categorize_model.py
+++ categorization/categorize_model.py
@@ -202,11 +202,26 @@ def categorize_model():
 
                         def exp_func(x, a, b): return a * np.exp(b * x)
                         binwidth = 5
-                        _hist_, _bins_ = np.histogram(MC_eval.loc[pass_sideband_mask, 'AUX_mass'].to_numpy(), bins=np.arange(100., 180., binwidth), weights=MC_eval.loc[pass_sideband_mask, 'AUX_eventWeight'].to_numpy())
+                        _hist_, _bins_ = np.histogram(
+                            MC_eval.loc[pass_sideband_mask, 'AUX_mass'].to_numpy(), 
+                            bins=np.arange(100., 180., binwidth), 
+                            weights=MC_eval.loc[pass_sideband_mask, 'AUX_eventWeight'].to_numpy()
+                        )
                         p0 = (_hist_[0], -0.1)
-                        params, _ = curve_fit(exp_func, _bins_[:-1]-_bins_[0], _hist_, p0=p0, sigma=np.where(np.isfinite(_hist_**-1), _hist_**-1, 0.76))
+                        params, _ = curve_fit(
+                            exp_func, _bins_[:-1]-_bins_[0], _hist_, p0=p0, 
+                            sigma=np.where(np.isfinite(_hist_**-1), _hist_**-1, 0.76)
+                        )
                         y_pred = exp_func(categorization_utils.SR_CUTS-_bins_[0], a=params[0], b=params[1])
                         est_yield = (categorization_utils.SR_CUTS[1] - categorization_utils.SR_CUTS[0]) * (y_pred[0] - 0.5*(y_pred[0] - y_pred[1]))
+                        categorization_utils.ascii_hist(
+                            MC_eval.loc[pass_sideband_mask, 'AUX_mass'].to_numpy(), 
+                            bins=np.arange(100., 180., binwidth), 
+                            weights=MC_eval.loc[pass_sideband_mask, 'AUX_eventWeight'].to_numpy(), 
+                            fit=exp_func(_bins_[:-1]-_bins_[0], a=params[0], b=params[1])
+                        )
+                        print(f"y = {params[0]:.2f}e^({params[1]:.2f}x)")
+                        print(f"  -> est. yield of non-res in SR = {est_yield}")
                         new_row.append(est_yield)
                     else:
                         new_row.append(MC_eval.loc[np.logical_and(pass_mask, MC_eval.loc[:, 'AUX_sample_name'].eq(sample_name).to_numpy()), 'AUX_eventWeight'].sum())
@@ -245,7 +260,7 @@ def categorize_model():
                 else:
                     pass_mask = np.logical_and(pass_mask, full_MC_eval.loc[:, TRANSFORM_COLUMNS[i]].lt(best_cut[i]).to_numpy())
             for sample_name in table.field_names[2:]:
-                if match_sample(sample_name, 'Data') is not None:
+                if match_sample(sample_name, ['Data']) is not None:
                     sideband_mask = np.logical_and(category_mask, categorization_utils.sideband_nonres_mask(full_MC_eval))
                     if '>' in TRANSFORM_CUT[i]:
                         pass_sideband_mask = np.logical_and(sideband_mask, full_MC_eval.loc[:, TRANSFORM_COLUMNS[i]].gt(best_cut[i]).to_numpy())
@@ -254,11 +269,26 @@ def categorize_model():
 
                     def exp_func(x, a, b): return a * np.exp(b * x)
                     binwidth = 5
-                    _hist_, _bins_ = np.histogram(full_MC_eval.loc[pass_sideband_mask, 'AUX_mass'].to_numpy(), bins=np.arange(100., 180., binwidth), weights=full_MC_eval.loc[pass_sideband_mask, 'AUX_eventWeight'].to_numpy())
+                    _hist_, _bins_ = np.histogram(
+                        full_MC_eval.loc[pass_sideband_mask, 'AUX_mass'].to_numpy(), 
+                        bins=np.arange(100., 180., binwidth), 
+                        weights=full_MC_eval.loc[pass_sideband_mask, 'AUX_eventWeight'].to_numpy()
+                    )
                     p0 = (_hist_[0], -0.1)
-                    params, _ = curve_fit(exp_func, _bins_[:-1]-_bins_[0], _hist_, p0=p0, sigma=np.where(np.isfinite(_hist_**-1), _hist_**-1, 0.76))
+                    params, _ = curve_fit(
+                        exp_func, _bins_[:-1]-_bins_[0], _hist_, p0=p0, 
+                        sigma=np.where(np.isfinite(_hist_**-1), _hist_**-1, 0.76)
+                    )
                     y_pred = exp_func(categorization_utils.SR_CUTS-_bins_[0], a=params[0], b=params[1])
                     est_yield = (categorization_utils.SR_CUTS[1] - categorization_utils.SR_CUTS[0]) * (y_pred[0] - 0.5*(y_pred[0] - y_pred[1]))
+                    categorization_utils.ascii_hist(
+                        full_MC_eval.loc[pass_sideband_mask, 'AUX_mass'].to_numpy(), 
+                        bins=np.arange(100., 180., binwidth), 
+                        weights=full_MC_eval.loc[pass_sideband_mask, 'AUX_eventWeight'].to_numpy(), 
+                        fit=exp_func(_bins_[:-1]-_bins_[0], a=params[0], b=params[1])
+                    )
+                    print(f"y = {params[0]:.2f}e^({params[1]:.2f}x)")
+                    print(f"  -> est. yield of non-res in SR = {est_yield}")
                     new_row.append(est_yield)
                 else:
                     new_row.append(full_MC_eval.loc[np.logical_and(pass_mask, full_MC_eval.loc[:, 'AUX_sample_name'].eq(sample_name).to_numpy()), 'AUX_eventWeight'].sum())
