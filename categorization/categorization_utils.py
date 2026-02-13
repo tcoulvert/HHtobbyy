fixing fit by normalization

categorization/categorization_utils.py
@@ -15,7 +15,7 @@ import hist
 
 
 SR_CUTS = [122.5, 127.]
-SIDEBAND_CUTS = [[100., 120.], [130., 180.]]
+FIT_BINS = [100., 180., 5.]  # start, stop, step
 
 ################################
 
@@ -30,19 +30,7 @@ def fom_mask(df: pd.DataFrame, SR_str: str='SR'):
     )
 
 def sideband_nonres_mask(df: pd.DataFrame, SB_str: str='SB'):
-    SB_mass_cut = np.logical_or(
-        np.logical_and(
-            df.loc[:, 'AUX_mass'].gt(SIDEBAND_CUTS[0][0]).to_numpy(), 
-            df.loc[:, 'AUX_mass'].lt(SIDEBAND_CUTS[0][1]).to_numpy()
-        ),
-        np.logical_and(
-            df.loc[:, 'AUX_mass'].gt(SIDEBAND_CUTS[1][0]).to_numpy(), 
-            df.loc[:, 'AUX_mass'].lt(SIDEBAND_CUTS[1][1]).to_numpy()
-        )
-    )
-    return np.logical_and(
-        SB_mass_cut, df.loc[:, 'cat_mask'].eq(SB_str).to_numpy()
-    )
+    return df.loc[:, 'cat_mask'].eq(SB_str).to_numpy()
 
 
 def fom_s_over_sqrt_b(s, b):
@@ -113,17 +101,17 @@ def brute_force(
 
             if sideband_fit:
                 _hist_ = hist.Hist(
-                    hist.axis.Regular((180-100)//5, 100, 180, name="var", growth=False, underflow=False, overflow=False), 
+                    hist.axis.Regular((int(FIT_BINS[1]-FIT_BINS[0])//FIT_BINS[2]), FIT_BINS[0], FIT_BINS[1], name="var", growth=False, underflow=False, overflow=False), 
                     storage='weight'
                 ).fill(var=bkg_sideband_mass[bkg_sideband_bool], weight=bkg_sideband_weights[bkg_sideband_bool])
                 params, _ = curve_fit(
                     exp_func, _hist_.axes.centers[0]-_hist_.axes.centers[0][0], _hist_.values(), p0=(_hist_.values()[0], -0.1), 
                     # sigma=np.where(_hist_.values() != 0, np.sqrt(_hist_.variances()), 0.76)
                 )
-                est_yield, _ = quad(exp_func, SR_CUTS[0]-_hist_.axes.centers[0][0], SR_CUTS[1]-_hist_.axes.centers[0][0], args=tuple(params))
+                est_yield = quad(exp_func, SR_CUTS[0]-_hist_.axes.centers[0][0], SR_CUTS[1]-_hist_.axes.centers[0][0], args=tuple(params))[0] / FIT_BINS[2]
                 print('='*60)
                 print(cuts[i])
-                ascii_hist(bkg_sideband_mass[bkg_sideband_bool], bins=np.arange(100., 160., 5.), weights=bkg_sideband_weights[bkg_sideband_bool], fit=exp_func(_hist_.axes.centers[0]-_hist_.axes.centers[0][0], a=params[0], b=params[1]))
+                ascii_hist(bkg_sideband_mass[bkg_sideband_bool], bins=np.arange(FIT_BINS[0], FIT_BINS[1], FIT_BINS[2]), weights=bkg_sideband_weights[bkg_sideband_bool], fit=exp_func(_hist_.axes.centers[0]-_hist_.axes.centers[0][0], a=params[0], b=params[1]))
                 print(f"y = {params[0]:.2f}e^({params[1]:.2f}x)")
                 print(f"  -> est. non-res bkg yield in SR = {est_yield}")
                 print(f"signal yield in SR = {np.sum(signal_sr_weights[signal_sr_bool])}, res bkg yield in SR = {np.sum(bkg_sr_weights[bkg_sr_bool])}, non-res bkg yield in SB = {np.sum(bkg_sideband_weights[bkg_sideband_bool])}")

categorization/categorize_model.py
@@ -150,7 +150,7 @@ CATEGORIZATION_METHOD = getattr(categorization_utils, CATEGORIZATION_OPTIONS['ME
 if 'STARTSTOPS' not in CATEGORIZATION_OPTIONS.keys():
     CATEGORIZATION_OPTIONS['STARTSTOPS'] = [[0., 1.] if '<' in TRANSFORM_CUT[i] else [1., 0.] for i in range(len(TRANSFORM_COLUMNS))]
 
-NONRES_MC_SAMPLENAMES = {'TTGG', 'GJet', 'GGJets', 'DDQCDGJets'}
+NONRES_MC_SAMPLENAMES = {'TTGG', 'GJet', 'GGJets', 'DDQCDGJets', 'SherpaNLO'}
 
 ################################
 
@@ -168,12 +168,18 @@ def categorize_model():
         assert set(cat_cols) <= set(MC_df.columns), f"The requested list of columns are not all in the file. Missing variables:\n{set(cat_cols) - set(MC_df.columns)}"
         MC_eval = MC_df.loc[:, cat_cols]
 
+        SHERPA_df, SHERPA_aux = get_test_subset_Dataframes(DATASET_DIRPATH, fold_idx, ['SherpaNLO'], minimal=MINIMAL)
+        for col in SHERPA_aux.columns: SHERPA_df.loc[:, col] = SHERPA_aux.loc[:, col]
+        assert set(cat_cols) <= set(SHERPA_df.columns), f"The requested list of columns are not all in the Sherpa file. Missing variables:\n{set(cat_cols) - set(SHERPA_df.columns)}"
+        # MC_eval = pd.concat([MC_eval.loc[MC_eval.loc[:, 'AUX_sample_name'].ne('GGJets').to_numpy()], SHERPA_df.loc[:, cat_cols]]).reset_index(drop=True)
+        MC_eval = pd.concat([MC_eval, SHERPA_df.loc[(SHERPA_df.loc[:, 'AUX_event'] % get_n_folds(DATASET_DIRPATH)).eq(fold_idx).to_numpy(), cat_cols]]).reset_index(drop=True)
+
         # DATA_df, DATA_aux = get_test_subset_Dataframes(DATASET_DIRPATH, fold_idx, ['!2024*Data'], minimal=MINIMAL)
         # DATA_aux['AUX_eventWeight'] = DATA_aux['AUX_eventWeight'] * 2.76  # lumi 22-24 / lumi 22-23
         DATA_df, DATA_aux = get_test_subset_Dataframes(DATASET_DIRPATH, fold_idx, ['Data'], minimal=MINIMAL)
         for col in DATA_aux.columns: DATA_df.loc[:, col] = DATA_aux.loc[:, col]
         assert set(cat_cols) <= set(DATA_df.columns), f"The requested list of columns are not all in the Data file. Missing variables:\n{set(cat_cols) - set(DATA_df.columns)}"
-        DATA_eval = DATA_df.loc[(DATA_df.loc[:, 'AUX_event'] % get_n_folds(DATASET_DIRPATH)).eq(fold_idx).to_numpy(), cat_cols]
+        DATA_eval = DATA_df.loc[(DATA_df.loc[:, 'AUX_event'] % get_n_folds(DATASET_DIRPATH)).eq(fold_idx).to_numpy(), cat_cols].reset_index(drop=True)
 
         if OPT_SIDEBAND != 'none':
             sample_cut = np.zeros(MC_eval.shape[0])
@@ -193,9 +199,7 @@ def categorize_model():
                 MC_eval.loc[:, 'cat_mask'] = np.where(~sample_cut, 'SR', '')
                 DATA_eval.loc[:, 'cat_mask'] = 'SB'
             if len(table.field_names) == 0: 
-                table.field_names = ['Category', 'FoM (s/b)'] + sorted([
-                    sample_name for sample_name in pd.unique(MC_eval['AUX_sample_name']).tolist() if sample_name not in NONRES_MC_SAMPLENAMES
-                ]) + ['nonRes MC -- SB fit', 'Data -- SB fit']
+                table.field_names = ['Category', 'FoM (s/b)'] + sorted(pd.unique(MC_eval['AUX_sample_name']).tolist()) + ['nonRes MC -- SB fit', 'Data -- SB fit']
         else: 
             MC_eval.loc[:, 'cat_mask'] = 'SR'
             DATA_eval.loc[:, 'cat_mask'] = ''
@@ -296,12 +300,6 @@ def categorize_model():
                 ] if cat_idx != 1 else None, sideband_fit=OPT_SIDEBAND != 'none'
             )
             new_row = [f'Merged folds - Cat {cat_idx}', best_fom]
-            pass_mask = np.logical_and(full_category_mask, categorization_utils.fom_mask(full_df_eval))
-            for i in range(len(TRANSFORM_COLUMNS)):
-                if '>' in TRANSFORM_CUT[i]:
-                    pass_mask = np.logical_and(pass_mask, full_df_eval.loc[:, TRANSFORM_COLUMNS[i]].gt(best_cut[i]).to_numpy())
-                else:
-                    pass_mask = np.logical_and(pass_mask, full_df_eval.loc[:, TRANSFORM_COLUMNS[i]].lt(best_cut[i]).to_numpy())
             for sample_name in table.field_names[2:]:
                 if match_sample(sample_name, ['Data', 'MC']) is not None:
                     pass_sideband_mask = np.logical_and(full_category_mask, categorization_utils.sideband_nonres_mask(full_df_eval, SB_str='SB' if match_sample(sample_name, [OPT_SIDEBAND]) is not None else ''))
@@ -313,21 +311,21 @@ def categorize_model():
 
                     def exp_func(x, a, b): return a * np.exp(b * x)
                     _hist_ = hist.Hist(
-                        hist.axis.Regular((180-100)//5, 100, 180, name="var", growth=False, underflow=False, overflow=False), 
+                        hist.axis.Regular(int((categorization_utils.FIT_BINS[1]-categorization_utils.FIT_BINS[0])//categorization_utils.FIT_BINS[2]), categorization_utils.FIT_BINS[0], categorization_utils.FIT_BINS[1], name="var", growth=False, underflow=False, overflow=False), 
                         storage='weight'
                     ).fill(var=full_df_eval.loc[pass_sideband_mask, 'AUX_mass'].to_numpy(), weight=full_df_eval.loc[pass_sideband_mask, 'AUX_eventWeight'].to_numpy())
                     params, _ = curve_fit(
                         exp_func, _hist_.axes.centers[0]-_hist_.axes.centers[0][0], _hist_.values(), p0=(_hist_.values()[0], -0.1), 
                         # sigma=np.where(_hist_.values() != 0, np.sqrt(_hist_.variances()), 0.76)
                     )
-                    est_yield, _ = quad(exp_func, categorization_utils.SR_CUTS[0]-_hist_.axes.centers[0][0], categorization_utils.SR_CUTS[1]-_hist_.axes.centers[0][0], args=tuple(params))
+                    est_yield = quad(exp_func, categorization_utils.SR_CUTS[0]-_hist_.axes.centers[0][0], categorization_utils.SR_CUTS[1]-_hist_.axes.centers[0][0], args=tuple(params))[0] / categorization_utils.FIT_BINS[2]
                     print('='*60+'\n'+'='*60)
                     print(sample_name)
                     print('-'*60)
                     print(best_cut)
                     categorization_utils.ascii_hist(
                         full_df_eval.loc[pass_sideband_mask, 'AUX_mass'].to_numpy(), 
-                        bins=np.arange(100., 180., 5.), 
+                        bins=np.arange(categorization_utils.FIT_BINS[0], categorization_utils.FIT_BINS[1], categorization_utils.FIT_BINS[2]), 
                         weights=full_df_eval.loc[pass_sideband_mask, 'AUX_eventWeight'].to_numpy(), 
                         fit=exp_func(_hist_.axes.centers[0]-_hist_.axes.centers[0][0], a=params[0], b=params[1])
                     )
@@ -336,6 +334,15 @@ def categorize_model():
                     print('='*60+'\n'+'='*60)
                     new_row.append(est_yield)
                 else:
+                    if OPT_SIDEBAND == 'none' or sample_name not in NONRES_MC_SAMPLENAMES: SR_str = 'SR'
+                    elif OPT_SIDEBAND == 'mc' and sample_name in NONRES_MC_SAMPLENAMES: SR_str = 'SB'
+                    elif OPT_SIDEBAND == 'data' and sample_name in NONRES_MC_SAMPLENAMES: SR_str = ''
+                    pass_mask = np.logical_and(full_category_mask, categorization_utils.fom_mask(full_df_eval, SR_str=SR_str))
+                    for i in range(len(TRANSFORM_COLUMNS)):
+                        if '>' in TRANSFORM_CUT[i]:
+                            pass_mask = np.logical_and(pass_mask, full_df_eval.loc[:, TRANSFORM_COLUMNS[i]].gt(best_cut[i]).to_numpy())
+                        else:
+                            pass_mask = np.logical_and(pass_mask, full_df_eval.loc[:, TRANSFORM_COLUMNS[i]].lt(best_cut[i]).to_numpy())
                     new_row.append(full_df_eval.loc[np.logical_and(pass_mask, full_df_eval.loc[:, 'AUX_sample_name'].eq(sample_name).to_numpy()), 'AUX_eventWeight'].sum())
             table.add_row(new_row)
 

preprocessing/retrieval_utils.py
@@ -162,13 +162,6 @@ def get_train_Dataframe(dataset_dirpath: str, fold_idx: int, dataset: str="train
             class_mask = aux['AUX_label1D'].eq(i)
             aux.loc[class_mask, 'AUX_eventWeightTrain'] = aux.loc[class_mask, 'AUX_eventWeightTrain'] * RES_BKG_RESCALE
 
-    # Sherpa: scale it to same as nonRes background
-    sherpa_mask = aux['AUX_sample_name'].eq('SherpaNLO')
-    if any(sherpa_mask):
-        # scale shepra by 1000, as mistake in cross-section fb vs pb
-        aux.loc[sherpa_mask, 'AUX_eventWeight'] *= 1000
-        aux.loc[sherpa_mask, 'AUX_eventWeightTrain'] *= 1000
-
     # Signal
     for i, key in enumerate(filepaths.keys()):
         if 'HH' in key:
@@ -251,6 +244,9 @@ def get_test_subset_Dataframes(dataset_dirpath: str, fold_idx: int, regexs: list
             if label:
                 data_idx = [i for i, key in enumerate(class_sample_map.keys()) if 'nonres' in key.lower()][0]
                 aux['AUX_label1D'] = data_idx * np.ones_like(aux['AUX_mass'])
+        elif match_sample(regex, ['SherpaNLO']) is not None and label:
+            sherpa_idx = [i for i, key in enumerate(class_sample_map.keys()) if 'nonres' in key.lower()][0]
+            aux['AUX_label1D'] = sherpa_idx * np.ones_like(aux['AUX_mass'])
         elif label:
             class_idx = [i for i, regex_list in enumerate(class_sample_map.values()) if regex in regex_list][0]
             aux['AUX_label1D'] = class_idx * np.ones_like(aux['AUX_mass'])