d9d9f6a1ce7fb502c8232d53dacbbf8318f419dd
Thomas Sievert <tcsievert@gmail.com>
2026-04-03 16:51:42 -0500

    working on improving dataset scripts

--- event_discrimination/models/MLP/mlp_dataset.py
+++ event_discrimination/models/MLP/mlp_dataset.py
@@ -4,14 +4,23 @@ import pandas as pd
 
 # ML packages
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
+
 
 from HHtobbyy.event_discrimination.dataset import DFDataset
 
 
 class MLP_Dataset(DFDataset):
-    def __init__(self, dfdataset: DFDataset):
+    def __init__(self, dfdataset: DFDataset, config: dict={}):
         super().__init__(dfdataset)
+
+        # Batch sizes for training
+        self.train_batch_size = 2048
+        self.val_batch_size = 2048
+        self.test_batch_size = 4096
+
+        # Number of workers for DataLoader
+        self.num_workers = 1
 
     #############################################################
     # Structure for pytorch dataset
@@ -43,7 +52,7 @@ class MLP_Dataset(DFDataset):
 
         if for_eval: event_weight = 'eventWeight'
         else: event_weight = 'eventWeightTrain'
-        return self.get_MLPTorch(train_df, event_weight)
+        return DataLoader(self.get_MLPTorch(train_df, event_weight), batch_size=self.train_batch_size, num_workers=self.num_workers, shuffle=not for_eval)
 
     def get_val(self, fold: int, syst_name: str='nominal', for_eval: bool=False):
         full_train_df = self.dfdataset.get_train(fold, syst_name=syst_name)
@@ -51,9 +60,9 @@ class MLP_Dataset(DFDataset):
 
         if for_eval: event_weight = 'eventWeight'
         else: event_weight = 'eventWeightTrain'
-        return self.get_MLPTorch(val_df, event_weight)
+        return DataLoader(self.get_MLPTorch(val_df, event_weight), batch_size=self.val_batch_size, num_workers=self.num_workers, shuffle=not for_eval)
 
     def get_test(self, fold: int, syst_name: str='nominal', regex: str=''):
         test_df = self.dfdataset.get_test(fold, syst_name=syst_name, regex=regex)
         
-        return self.get_MLPTorch(test_df, 'eventWeight')
+        return DataLoader(self.get_MLPTorch(test_df, 'eventWeight'), batch_size=self.test_batch_size, num_workers=self.num_workers, shuffle=False)
