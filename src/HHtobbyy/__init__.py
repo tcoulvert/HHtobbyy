2b98ee27b996310c3f374ff28dc442a2c3783f62
Thomas Sievert <tcsievert@gmail.com>
2026-04-11 19:29:17 -0500

    working on categorization

--- src/HHtobbyy/__init__.py
+++ src/HHtobbyy/__init__.py
@@ -1,3 +1,2 @@
-# from HHtobbyy import FinalFit, Categorization, event_discrimination, preprocessing
-from HHtobbyy import FinalFit, event_discrimination, preprocessing
+from HHtobbyy import FinalFit, Categorization, event_discrimination, preprocessing
 from HHtobbyy import workspace_utils
